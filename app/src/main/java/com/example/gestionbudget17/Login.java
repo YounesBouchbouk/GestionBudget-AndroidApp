16794269cd1c6311d62d583188d315c02e8a321f
younesbouchbouk <younesbouchbouk.py@gmail.com>
2021-12-03 21:58:36 +0100

    Dashboard xml1

--- app/src/main/java/com/example/gestionbudget17/Login.java
+++ app/src/main/java/com/example/gestionbudget17/Login.java
@@ -9,6 +9,6 @@ public class Login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_dashboard);
     }
 }
